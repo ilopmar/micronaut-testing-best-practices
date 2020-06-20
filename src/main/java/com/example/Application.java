4a8ffeed5e9fbf1743c7de021b41ad72520e167c
Iván López <lopez.ivan@gmail.com>
2020-06-20 17:41:42 +0200

    Extra: Add OpenAPI

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,7 +1,15 @@
 package com.example;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "demo",
+                version = "0.1"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
